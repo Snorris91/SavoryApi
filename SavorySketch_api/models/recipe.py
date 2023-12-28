1be1fa77d4810d05075c697f8bdea4cda2d9064b
Spencer Norris <Spencer.serv@gmail.com>
2023-12-28 13:27:48 -0600

    update

--- SavorySketch_api/models/recipe.py
+++ SavorySketch_api/models/recipe.py
@@ -5,7 +5,7 @@ class Recipe(models.Model):
     cuisine = models.ForeignKey("Cuisine", on_delete=models.CASCADE, related_name="recipe_cuisine")
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-    publication_date = models.DateField(auto_now=True)
+    publication_date = models.DateField(auto_now_add=True)
     image = models.URLField(null=True, blank=True)
     directions = models.CharField(max_length=2000)
     number_of_likes = models.IntegerField(default=0)
